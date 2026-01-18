5b367e46e7766f8f44c6621d90c53b6cbc299411
Pawel <pawelratajczak1998@gmail.com>
2026-01-18 04:37:46 +0100

    Recipe Revert

--- Core/Entities/Recipe.cs
+++ Core/Entities/Recipe.cs
@@ -1,17 +1,21 @@
-﻿namespace Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities
 {
     public class Recipe
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }     
+        public string Description { get; set; }
         public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
         public ICollection<Category> Categories { get; set; } = new List<Category>();
         public ICollection<Comment> Comments { get; set; } = new List<Comment>();
         public ICollection<Like> Likes { get; set; } = new List<Like>();
         public decimal TotalCost { get; set; } = 0m;
         public ICollection<Rating> Ratings { get; set; } = new List<Rating>();
-        public decimal AverageRating { get; set; } = 0m;
+        public decimal AverageRating =>
+            Ratings.Count > 0 ? Math.Round(Ratings.Average(r => (decimal)r.Value), 2) : 0m;
+
         public string Instructions { get; set; }
         public TimeSpan PreparationTime { get; set; }
         public TimeSpan CookingTime { get; set; }
@@ -21,6 +25,6 @@
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public string? ImageUrl { get; set; }
-            
+
     }
 }
